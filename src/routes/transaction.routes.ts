6323cd91bfb190e136da9ee530fe4813e6dc5d60
lbastoss <lucas.bastos.7@hotmail.com>
2025-09-08 11:31:00 -0300

    zood nas rotas

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,11 +1,15 @@
 import type { FastifyInstance } from "fastify";
+import { zodToJsonSchema } from "zod-to-json-schema";
 import createTransaction from "../controllers/transactions/createTransaction.controller";
+import { createTransactionSchema } from "../schemas/transaction.schema";
 
 const transactionRoutes = async (fastify: FastifyInstance) => {
   fastify.route({
     method: "POST",
     url: "/",
-    schema: {},
+    schema: {
+      body: zodToJsonSchema(createTransactionSchema),
+    },
     handler: createTransaction,
   });
 };
