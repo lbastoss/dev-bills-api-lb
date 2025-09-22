536b9597ea93473f3f0dca8eb9e93eadfb0b9fe6
lbastoss <lucas.bastos.7@hotmail.com>
2025-09-22 14:35:54 -0300

    iniciando resumo das transações

--- src/controllers/transactions/getTransactionSummary.controller.ts
+++ src/controllers/transactions/getTransactionSummary.controller.ts
@@ -1,8 +1,11 @@
+import { TransactionType } from "@prisma/client";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import prisma from "../../config/prisma";
 import type { GetTransactionsSummaryQuery } from "../../schemas/transaction.schema";
+import type { CategorySummary } from "../../types/category.types";
+import type { TransactionSummary } from "../../types/transaction.types";
 
 dayjs.extend(utc);
 
@@ -40,9 +43,42 @@ export const getTransactionsSummary = async (
       },
     });
 
-    console.log(transactions);
+    let totalExpenses = 0;
+    let totalIncomes = 0;
+    const groupedExpenses = new Map<string, CategorySummary>();
 
-    reply.send(transactions);
+    for (const transaction of transactions) {
+      if (transaction.type === TransactionType.expense) {
+        const existing = groupedExpenses.get(transaction.categoryId) ?? {
+          categoryId: transaction.categoryId,
+          categoryName: transaction.category.name,
+          categoryColor: transaction.category.color,
+          amount: 0,
+          percentage: 0,
+        };
+
+        existing.amount += transaction.amount;
+        groupedExpenses.set(transaction.categoryId, existing);
+
+        totalExpenses += transaction.amount;
+      } else {
+        totalIncomes += transaction.amount;
+      }
+    }
+
+    const summary: TransactionSummary = {
+      totalExpenses,
+      totalIncomes,
+      balance: totalIncomes - totalExpenses,
+      expensesByCategory: Array.from(groupedExpenses.values())
+        .map((entry) => ({
+          ...entry,
+          percentage: Number.parseFloat(((entry.amount / totalExpenses) * 100).toFixed(2)),
+        }))
+        .sort((a, b) => b.amount - a.amount),
+    };
+
+    reply.send(summary);
   } catch (err) {
     request.log.error(err, "Error servidor:");
     reply.status(500).send({ error: "Server error" });
